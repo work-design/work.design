c5ac0c510856b9fad5278415020e4180f61cbd21
qinmingyuan <mingyuan0715@foxmail.com>
2020-12-31 10:47:58 +0800

    add set host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
 
   config.action_cable.disable_request_forgery_protection = true
   config.action_cable.allowed_request_origins = [
-    'https://one.work'
+    '*'
   ]
 
   config.active_job.queue_adapter = :sidekiq
