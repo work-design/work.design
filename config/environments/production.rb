6e11e49b3413493216ca97296eb622699ca210af
qinmingyuan <mingyuan0715@foxmail.com>
2021-02-21 23:38:14 +0800

    not proxy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,6 @@ Rails.application.configure do
   config.public_file_server.enabled = true
 
   config.active_storage.service = :upyun
-  config.active_storage.resolve_model_to_route = :rails_storage_proxy
 
   config.action_mailer.smtp_settings = {
     address: 'smtp.exmail.qq.com',
