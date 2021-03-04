22a10d22d468e51327bb96840226b60786a4454d
qinmingyuan <mingyuan0715@foxmail.com>
2021-03-04 21:58:59 +0800

    role

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_140912) do
+ActiveRecord::Schema.define(version: 2021_03_04_135018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -543,6 +543,7 @@ ActiveRecord::Schema.define(version: 2021_02_25_140912) do
     t.integer "position", scale: 4
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
   create_table "cache_lists", id: { scale: 8 }, force: :cascade do |t|
