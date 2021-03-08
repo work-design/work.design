1e187fab5deb963b505d6c5bc1b7dc94de032672
qinmingyuan <mingyuan0715@foxmail.com>
2021-03-08 11:32:47 +0800

    templay disable content security policy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,6 @@ class ApplicationController < ActionController::Base
 
   #before_action :support_organ
   protect_from_forgery with: :exception, unless: -> { json_format? }
-  #content_security_policy true
+  content_security_policy false
 
 end
