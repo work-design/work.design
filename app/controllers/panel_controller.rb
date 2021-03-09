6c0550fae0253d66e8ffe6e165f6a5fc6c9c6a58
qinmingyuan <mingyuan0715@foxmail.com>
2021-03-09 11:43:28 +0800

    move

--- app/controllers/panel_controller.rb
+++ app/controllers/panel_controller.rb
@@ -1,7 +1,7 @@
 class PanelController < ApplicationController
   before_action :require_login
   before_action :require_role
-  after_action :set_flash, only: [:update, :create, :destroy]
+  #after_action :set_flash, only: [:update, :create, :destroy]
 
   def rails_role_user
     current_user
