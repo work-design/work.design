b7a3161af6d4202f5a3c088ebc67afe188ddd26d
qinmingyuan <mingyuan0715@foxmail.com>
2021-03-28 00:53:06 +0800

    status for sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -35,7 +35,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   require 'sidekiq/web'
-  constraints ->(req) { AuthorizedToken.find_by(token: req.env['rack.session']['auth_token'])&.user&.admin? if req.env['rack.session'].present? } do
+  constraints ->(req) { Auth::AuthorizedToken.find_by(token: req.env['rack.session']['auth_token'])&.user&.admin? if req.env['rack.session'].present? } do
     mount Sidekiq::Web => '/sidekiq'
   end
   mount ActionCable.server => '/cable'
