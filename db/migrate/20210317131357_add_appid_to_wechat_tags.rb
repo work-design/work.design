277f3b3d55f22c040bea0ef43e06f20f8bd553ee
MingyuanQin <mingyuan0715@foxmail.com>
2021-03-31 11:13:39 +0800

    rename

--- db/migrate/20210317131357_add_appid_to_wechat_tags.rb
+++ db/migrate/20210317131357_add_appid_to_wechat_tags.rb
@@ -1,8 +1,8 @@
 class AddAppidToWechatTags < ActiveRecord::Migration[6.1]
   def change
     add_column :wechat_tags, :appid, :string, index: true
-    Wechat::WechatTag.find_each do |i|
-      i.appid = Wechat::WechatApp.find_by(id: i.wechat_app_id)&.appid
+    Wechat::Tag.find_each do |i|
+      i.appid = Wechat::App.find_by(id: i.wechat_app_id)&.appid
       i.save
     end
     remove_column :wechat_tags, :wechat_app_id
