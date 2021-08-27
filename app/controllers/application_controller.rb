c2ed1dfab87b8edd013db3d5f4ce27568497fa86
qinmingyuan <mingyuan0715@foxmail.com>
2021-08-27 21:36:48 +0800

    gemfile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   include Auth::Controller::Application
   include Wechat::Controller::Application
   include RailsCom::Application
+  include Com::Controller::Admin
   include Roled::Controller::Application
   include Org::Controller::Application
   include Org::Controller::Me
