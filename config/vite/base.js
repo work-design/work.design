daadff0e44214ee36899dd2a8a96da3a589b91c8
qinmingyuan <mingyuan0715@foxmail.com>
2021-09-20 23:35:12 +0800

    vite

--- config/vite/base.js
+++ config/vite/base.js
@@ -1,9 +1,9 @@
 const { viteConfig } = require('vite_rails')
 
 module.exports = {
-  // define: {
-  //   'process.env': {},
-  //   __dirname: {}
-  // },
+  define: {
+    'process.env': {},
+    __dirname: {}
+  },
   ...viteConfig
 }
--- config/vite/production.js
+++ config/vite/production.js
@@ -1,4 +1,5 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
+process.env.RAILS_ENV = process.env.NODE_ENV
 
 const viteConfig = require('./base')
 
