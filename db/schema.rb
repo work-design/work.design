f5d2c88d381c87f0035afef7676116c629584f0d
qinmingyuan <mingyuan0715@foxmail.com>
2022-10-09 22:27:38 +0800

    sha

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_09_124106) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_09_142559) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -2564,6 +2564,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_09_124106) do
     t.string "download_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "sha"
     t.index ["git_id"], name: "index_markdown_assets_on_git_id"
   end
 
