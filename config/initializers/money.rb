366ace84ae3db37ef6e3728f240a18c044b4e20b
qinmingyuan <mingyuan0715@foxmail.com>
2023-07-14 12:14:26 +0800

    improve show money

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -1,1 +1,6 @@
 Money.default_currency = Money::Currency.new('CNY')
+Money.locale_backend = :i18n
+Money.rounding_mode = BigDecimal::ROUND_HALF_UP
+Money.default_formatting_rules = {
+  html_wrap: true
+}
