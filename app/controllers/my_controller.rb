b0a28894613321a83acd8fe8c544827594a3ce5d
qinmingyuan <mingyuan0715@foxmail.com>
2023-07-15 23:29:50 +0800

    check items

--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -1,6 +1,7 @@
 class MyController < ApplicationController
+  include Org::Controller::Me
+  include Com::Controller::Admin
   include Trade::Controller::My
   before_action :require_user
-  #before_action :require_session_organ
 
 end
