378a8c7001c59b0b6b7f846418f2662396e659f9
qinmingyuan <mingyuan0715@foxmail.com>
2023-07-15 10:17:53 +0800

    fix work design

---
+++ app/controllers/my/home_controller.rb
@@ -0,0 +1,9 @@
+class My::HomeController < My::BaseController
+  before_action :set_card_templates, :set_wallet_template, only: [:index]
+  before_action :set_lawful_wallet, only: [:index]
+  before_action :set_cart, only: [:index]
+
+  def index
+  end
+
+end
--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -1,4 +1,5 @@
 class MyController < ApplicationController
+  include Trade::Controller::My
   before_action :require_user
   #before_action :require_session_organ
 
