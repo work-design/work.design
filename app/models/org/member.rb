263ebe6c1b0e13878c1311aa9d13feec3b08777f
qinmingyuan <mingyuan0715@foxmail.com>
2023-07-17 19:31:31 +0800

    support crm

--- app/models/org/member.rb
+++ app/models/org/member.rb
@@ -2,6 +2,7 @@ module Org
   class Member < ApplicationRecord
     include Model::Member
     include Bench::Ext::Member
+    include Crm::Ext::Member
     include Roled::Ext::Member
     include Trade::Ext::Member
     include Finance::Ext::Member
