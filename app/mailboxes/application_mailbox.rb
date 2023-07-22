7fbc273a61eae90649da8b5d22ccb8ff9b4a2c18
qinmingyuan <mingyuan0715@foxmail.com>
2023-07-23 01:53:01 +0800

    gemfiele

--- app/mailboxes/application_mailbox.rb
+++ app/mailboxes/application_mailbox.rb
@@ -1,4 +1,4 @@
 class ApplicationMailbox < ActionMailbox::Base
   routing ->(email) { email.mail.from.end_with?('<weixinmphelper@tencent.com>') } => :wechat
-  routing /@mail.work.design/i => :attachment
+  routing /@mail\.work\.design/i => :attachment
 end
