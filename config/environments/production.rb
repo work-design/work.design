faa62f87c8e1e3226bfb75faa24c07dc46358ec9
qinmingyuan <mingyuan0715@foxmail.com>
2023-07-23 01:06:54 +0800

    the logic for postmark

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,6 +36,8 @@ Rails.application.configure do
 
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailbox.ingress = :postmark
+
   config.action_cable.disable_request_forgery_protection = true
   config.action_cable.allowed_request_origins = [
     '*'
