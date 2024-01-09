c3d6990bf7994f746d188b0f2d64311882b92f22
qinmingyuan <mingyuan0715@foxmail.com>
2024-01-09 09:32:57 +0800

    suppor changes

--- app/models/trade/item.rb
+++ app/models/trade/item.rb
@@ -4,7 +4,8 @@ module Trade
     include Job::Ext::Jobbed
     include Ship::Ext::Item
     include Bench::Ext::Item
-    include Factory::Ext::Item
+    include Factory::Ext::ItemGood
+    include Factory::Ext::ItemPurchase
     include Space::Ext::Item
     include JiaBo::Ext::Device
     include Crm::Ext::Maintainable
