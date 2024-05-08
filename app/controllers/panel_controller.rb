5ea6e62cdfd75e4009547c822e446c1bbee5bbcd
qinmingyuan <mingyuan0715@foxmail.com>
2024-05-08 23:42:09 +0800

    d

--- app/controllers/panel_controller.rb
+++ app/controllers/panel_controller.rb
@@ -1,5 +1,6 @@
 class PanelController < ApplicationController
   include Com::Controller::Panel
+  include Roled::Controller::Panel
   before_action :require_member_or_user
   before_action :require_role
   #after_action :set_flash, only: [:update, :create, :destroy]
