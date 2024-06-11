385050907fbee359782dc7421b5470d90698c937
qinmingyuan <mingyuan0715@foxmail.com>
2024-06-11 15:52:56 +0800

    qingflows

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_06_08_163523) do
+ActiveRecord::Schema[7.2].define(version: 2024_06_11_073826) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -3724,9 +3724,20 @@ ActiveRecord::Schema[7.2].define(version: 2024_06_08_163523) do
     t.datetime "updated_at", null: false
     t.bigint "application_id"
     t.bigint "meta_column_id"
+    t.boolean "display"
     t.index ["application_id"], name: "index_qingflow_forms_on_application_id"
     t.index ["meta_column_id"], name: "index_qingflow_forms_on_meta_column_id"
     t.index ["parent_id"], name: "index_qingflow_forms_on_parent_id"
+  end
+
+  create_table "qingflow_items", force: :cascade do |t|
+    t.bigint "application_id"
+    t.string "applyid"
+    t.jsonb "answers"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["application_id"], name: "index_qingflow_items_on_application_id"
+    t.index ["applyid"], name: "index_qingflow_items_on_applyid"
   end
 
   create_table "quip_apps", force: :cascade do |t|
