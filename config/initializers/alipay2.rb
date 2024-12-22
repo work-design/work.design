47237f79599557621e39d886baf274b3f4293eb7
qinmingyuan <mingyuan0715@foxmail.com>
2024-12-22 15:32:45 +0800

    add alipay

--- config/initializers/alipay2.rb
+++
@@ -1,8 +0,0 @@
-Alipay2.configure do |config|
-  config.sandbox = true if Rails.env.development?
-  config.appid = Rails.application.credentials.dig(:alipay, :appid)
-  config.target_id = '20141225xxxx'
-  config.notify_url = Rails.application.credentials.dig(:alipay, :alipay_notify_url)
-  config.oauth_url = Rails.application.credentials.dig(:alipay, :alipay_oauth_url)
-  config.oauth_callback = Rails.application.credentials.dig(:alipay, :alipay_oauth_callback)
-end
