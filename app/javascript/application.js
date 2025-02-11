27ca0b120732755ea81ca21223e3d5d63fd05c21
qinmingyuan <mingyuan0715@foxmail.com>
2025-02-11 20:09:20 +0800

    dail

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,2 @@
 import 'rails_design'
-import 'rails_design/channels'
 application.debug = true
--- app/javascript/phone.js
+++ app/javascript/phone.js
@@ -1,4 +1,3 @@
 import 'rails_design'
-import 'rails_design/channels'
 import 'rails_design/stimulus_weui'
 application.debug = true
